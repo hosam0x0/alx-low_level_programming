cd4a383c040af634580c7d6f825e2f12a1c38bdc
Hosameldeen <zerohosam22@gmail.com>
2022-10-17 17:56:49 -0700

    4 task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.c>
+/**
+ * main -  prints alphabet in lowercase
+ * followd by a new line, except q & e
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+{
+	for (la = 'a'; la <= 'z'; la++)
+	}
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+	putchar('\n');
+	return (0);
+}
+
