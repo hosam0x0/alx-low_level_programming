3dd1f8818fb18299a4746b597a5d98ba2b27fe19
Hosameldeen <zerohosam22@gmail.com>
2022-10-19 19:27:03 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,12 +4,12 @@
  */
 void times_table(void)
 {
-	int num, multi, prod;
+	int num, mult, prod;
 
 	for (num = 0; num <= 9; num++)
 		_putchar('0');
 
-		for (multi = 1; multi <= 9; multi++)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
 			_putchar(' ');
