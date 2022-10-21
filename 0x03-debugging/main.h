3951d703026f22b03e50d3510ee0db4fccc88685
Hosameldeen <zerohosam22@gmail.com>
2022-10-21 16:33:25 -0700

    0 task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stdio>
+#include <stdio.h>
 void positive_or_negative(int 1);
 int largest_number(int a; int b; int c);
 void print_remaining_days(int month, int day, int year);
