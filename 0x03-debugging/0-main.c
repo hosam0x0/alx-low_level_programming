12c7dd362e92329f190f46ff1835e34b9c36b867
Hosameldeen <zerohosam22@gmail.com>
2022-10-21 15:55:49 -0700

    0 task

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - tests function that prints if integer is postive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int 1;
+
+	i = 0;
+	positive_or_negative(1);
+
+	return (0);
+}
